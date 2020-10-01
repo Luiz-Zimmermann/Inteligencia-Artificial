bbe852c37c8745e8748673f89e0d21e309d7b451
Lucas J. Cunha <lucasgb7_ks@hotmail.com>
2020-09-30 21:45:38 -0300

    add entradas/saidas e regras

--- Fuzzy/main.py
+++ Fuzzy/main.py
@@ -3,109 +3,136 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skfuzzy import control as ctrl
 
-
 # Tamanho do gráfico (eixo X)
+lenght = np.arange(0, 11, 1)
+direcao_fixa = "Direita"
+
 # Entradas
-velocidade = np.arange(0,11,1)
+velocidade_entrada = np.arange(0,11,1)
+proximidade = np.arange(0,11,1)
 posicao = np.arange(0,11,1)
 
 # Saídas
 direcao = np.arange(0,11,1)
+velocidade_saida = np.arange(0,11,1)
 
 #### Fuzzyficação ####
 # trimf = triângulo, trampf = trapézio
-# Velocidade: low = baixa; medium = normal; high = alta
-# Definir valores para l,n e h no trabalho
-vel_l = fzy.trapmf(velocidade, [0,0,2,4])
-vel_m = fzy.trimf(velocidade, [3,5,7])
-vel_h = fzy.trapmf(velocidade, [6,8,10,10])
+# Velocidade de entrada: low = baixa; medium = normal; high = alta
+vel_en_l = fzy.trapmf(velocidade_entrada, [0,0,2,4])
+vel_en_m = fzy.trimf(velocidade_entrada, [3,5,7])
+vel_en_h = fzy.trapmf(velocidade_entrada, [6,8,10,10])
 
-# Posição: low = esquerda; medium = centro; high = direita
-# Definir valores para l,n e h no trabalho
+# Proximidade: close = perto; medium = normal; far = longe
+pro_c = fzy.trapmf(proximidade, [0,0,2,4])
+pro_m = fzy.trimf(proximidade, [3,5,7])
+pro_f = fzy.trapmf(proximidade, [6,8,10,10])
+
+# Posição: left = esquerda; center = centro; right = direita
 pos_l = fzy.trimf(posicao, [0,0,5])
-pos_m = fzy.trimf(posicao, [4,5,6])
-pos_h = fzy.trimf(posicao, [5,10,10])
+pos_c = fzy.trimf(posicao, [4,5,6])
+pos_r = fzy.trimf(posicao, [5,10,10])
 
 # Direção: low = esquerda; medium = centro; high = direita
-# Definir valores para l,n e h no trabalho
 direct_l = fzy.trimf(direcao, [0,0,5])
 direct_m = fzy.trimf(direcao, [4,5,6])
 direct_h = fzy.trimf(direcao, [5,10,10])
 
+# Velocidade de saida: low = baixa; medium = normal; high = alta
+vel_sa_l = fzy.trapmf(velocidade_saida, [0,0,2,4])
+vel_sa_m = fzy.trimf(velocidade_saida, [3,5,7])
+vel_sa_h = fzy.trapmf(velocidade_saida, [6,8,10,10])
 
-"""
-fig, (ax0, ax1) = plt.subplots(nrows=2, figsize=(8, 9))
+# Define as entradas e saidas do sistema
+velocidade_entrada = ctrl.Antecedent(velocidade_entrada, "Velocidade de Entrada")
+proximidade = ctrl.Antecedent(proximidade, "Proximidade")
+posicao = ctrl.Antecedent(posicao, "Posição")
+direcao = ctrl.Consequent(direcao, "Direção")
+velocidade_saida = ctrl.Consequent(velocidade_saida, "Velocidade de Saida")
 
-ax0.plot(velocidade, vel_l, 'b', linewidth=1.5, label='Baixa')
-ax0.plot(velocidade, vel_m, 'g', linewidth=1.5, label='Normal')
-ax0.plot(velocidade, vel_h, 'r', linewidth=1.5, label='Alta')
-ax0.set_title('Velocidade')
-ax0.legend()
+# Faixa de valores para utilizar nas regras
+velocidade_entrada["Baixa"] = vel_en_l
+velocidade_entrada["Normal"] = vel_en_m
+velocidade_entrada["Alta"] = vel_en_h
 
-ax1.plot(direcao, direc_l, 'b', linewidth=1.5, label='Esquerda')
-ax1.plot(direcao, direc_m, 'g', linewidth=1.5, label='Centro')
-ax1.plot(direcao, direc_h, 'r', linewidth=1.5, label='Direita')
-ax1.set_title('Direção')
-ax1.legend()
-
-
-# Turn off top/right axes
-for ax in (ax0, ax1):
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
-plt.tight_layout()
-plt.show()
-"""
-
-velocidade = ctrl.Antecedent(velocidade,"Velocidade")
-posicao = ctrl.Antecedent(posicao,"Posição")
-direcao = ctrl.Consequent(direcao,"Direção")
-
-
-velocidade["Baixa"] = vel_l
-velocidade["Normal"] = vel_m
-velocidade["Alta"] = vel_h
+proximidade["Perto"] = pro_c
+proximidade["Normal"] = pro_m
+proximidade["Longe"] = pro_f
 
 posicao["Esquerda"] = pos_l
-posicao["Centro"] = pos_m
-posicao["Direita"] = pos_h
+posicao["Centro"] = pos_c
+posicao["Direita"] = pos_r
 
 direcao["Esquerda"] = direct_l
 direcao["Centro"] = direct_m
 direcao["Direita"] = direct_h
 
-velocidade.view()
+velocidade_saida["Baixa"] = vel_sa_l
+velocidade_saida["Normal"] = vel_sa_m
+velocidade_saida["Alta"] = vel_sa_h
+
+# Plota os graficos das entradas e saidas
+velocidade_entrada.view()
+proximidade.view()
 posicao.view()
 direcao.view()
+velocidade_saida.view()
 
-regra1 = ctrl.Rule(velocidade["Alta"] & posicao["Esquerda"], direcao["Direita"])
-regra2 = ctrl.Rule(velocidade["Alta"] & posicao["Direita"], direcao["Esquerda"])
-regra3 = ctrl.Rule(velocidade["Alta"] & posicao["Centro"], direcao["Direita"])
+# Definição das regras
+regra1 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Baixa"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Baixa"])
+regra2 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Baixa"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Baixa"])
+regra3 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Baixa"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Baixa"])
 
-regra4 = ctrl.Rule(velocidade["Normal"] & posicao["Esquerda"], direcao["Direita"])
-regra5 = ctrl.Rule(velocidade["Normal"] & posicao["Direita"], direcao["Esquerda"])
-regra6 = ctrl.Rule(velocidade["Normal"] & posicao["Centro"], direcao["Direita"])
+regra4 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Normal"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Baixa"])
+regra5 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Normal"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Baixa"])
+regra6 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Normal"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Baixa"])
 
-regra7 = ctrl.Rule(velocidade["Baixa"] & posicao["Esquerda"], direcao["Direita"])
-regra8 = ctrl.Rule(velocidade["Baixa"] & posicao["Direita"], direcao["Esquerda"])
-regra9 = ctrl.Rule(velocidade["Baixa"] & posicao["Centro"], direcao["Direita"])
+regra7 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Alta"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Baixa"])
+regra8 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Alta"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Baixa"])
+regra9 = ctrl.Rule(proximidade["Perto"] & velocidade_entrada["Alta"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Baixa"])
 
 
-print(regra1)
-print(regra2)
-print(regra3)
+regra10 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Baixa"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Normal"])
+regra11 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Baixa"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Normal"])
+regra12 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Baixa"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Normal"])
 
-controle = ctrl.ControlSystem([regra1,regra2,regra3,regra4,regra5,regra6,regra7,regra8,regra9])
+regra13 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Normal"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Normal"])
+regra14 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Normal"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Normal"])
+regra15 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Normal"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Normal"])
+
+regra16 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Alta"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Normal"])
+regra17 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Alta"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Normal"])
+regra18 = ctrl.Rule(proximidade["Normal"] & velocidade_entrada["Alta"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Normal"])
+
+
+regra19 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Baixa"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Normal"])
+regra20 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Baixa"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Normal"])
+regra21 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Baixa"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Normal"])
+
+regra22 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Normal"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Normal"])
+regra23 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Normal"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Normal"])
+regra24 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Normal"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Normal"])
+
+regra25 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Alta"] & posicao["Esquerda"], direcao["Direita"] & velocidade_saida["Normal"])
+regra26 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Alta"] & posicao["Centro"], direcao[direcao_fixa] & velocidade_saida["Normal"])
+regra27 = ctrl.Rule(proximidade["Longe"] & velocidade_entrada["Alta"] & posicao["Direita"], direcao["Esquerda"] & velocidade_saida["Normal"])
+
+regra28 = ctrl.Rule(None, velocidade_saida["Alta"])
+# --------------------------------------------------------------- #
+lista_regras = [regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8, regra9,
+                regra10, regra11, regra12, regra13, regra14, regra15, regra16, regra17, regra18,
+                regra19, regra20, regra21, regra22, regra23, regra24, regra25, regra26, regra27,
+                regra28]
+controle = ctrl.ControlSystem(lista_regras)
 simulacao = ctrl.ControlSystemSimulation(controle)
 
-simulacao.input["Velocidade"] = 3
-simulacao.input["Posição"] = 5
+# simulacao.input["Velocidade de Entrada"] = 3
+# simulacao.input["Posição"] = 5
 simulacao.compute()
 
 print(simulacao.output["Direção"])
 direcao.view(sim=simulacao)
+print(simulacao.output["Velocidade de Saida"])
+velocidade_saida.view(sim=simulacao)
 
 plt.show()
