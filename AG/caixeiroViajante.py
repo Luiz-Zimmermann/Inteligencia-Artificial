21138b0baff3f824ee6dd03b4d0bb93f3ff0d53c
Luiz Zimmermann <luizzimmermann@edu.univali.br>
2020-12-03 18:31:02 -0300

    Trabalho quase quase pronto. Geração, avaliação, mutação, evolução prontos

--- AG/caixeiroViajante.py
+++ AG/caixeiroViajante.py
@@ -8,31 +8,36 @@ Algoritmo Genetico -> Solucao para problema do Caixeiro Viajante
 from numpy import *
 from matplotlib.pyplot import *
 from csv import *
-from cromossomo import *
+from AG.cromossomo import *
 import random
 
-# TODO retornar a distancia total do percurso de acordo com as cidades
-def search_table(rota):
-    pass
+def population_generator(populacao, cities, length):
 
-
-def population_generator(populacao, cities):
-
-    for i in range(population):
+    for i in range(length):
         # Cria um cromossomo, cada gene é uma cidade
         genes = np.array(random.sample(list(cities), len(cities)))
-        cromossomo = Cromossomo(genes, sum(genes) * 2)
+        cromossomo = Cromossomo(genes, getDistance(genes, distance))
         populacao.append(cromossomo)
+
+    return populacao
+
+def evaluation(populacao):
+
+    populacao = sorted(populacao, key=lambda cromossomo: cromossomo.distanciaTotal)
+    if len(populacao) >= 2:
+        nova_populacao = populacao[:len(populacao)//2]
+        return nova_populacao
+    return populacao
 
 def exists(rna_1, rna_2):
     for gene in rna_1:
-        print(gene)
         if gene in rna_2:
             return True
     return False
 
-# TODO terminar a geração de cromossomos
+
 def mutation(populacao):
+
     new_population = []
     population_len = len(populacao)
     dna_length = len(populacao[0].rotas)
@@ -42,54 +47,87 @@ def mutation(populacao):
     print("DNA: ", dna_length)
     print("RNA: ", rna_length)
     print("RNA gap: ", rna_gap)
-    print("Tamanho da população antes da mutação: ", len(populacao))
-
 
     # Embaralha a população
     populacao = random.sample(populacao, len(populacao))
+    iterations = 0
+    while not len(populacao) <= 1:
+        done = False
+        limit = len(populacao)
+        while not done and not iterations >= limit*2:
 
-    #for parents in arange(populacao,step=2):
-    done = False
-    index1 = 0
-    index2 = 0
-    while not done:
-        # Escolha dos pais
-        parent1 = populacao.pop(index1)
-        parent2 = populacao.pop(index2)
-        parent1_rna = parent1.rotas[:rna_length]
-        parent2_rna = parent2.rotas[rna_gap-1:]
-        if not exists(parent1_rna, parent2_rna):
+            # Escolha dos pais
+            parents = random.sample(populacao, 2)
+            parent1 = parents[0]
+            parent2 = parents[1]
 
-            print("Pai 1 e 2: ", parent1.rotas, parent2.rotas)
-            print("Filho: ", parent1_rna, parent2_rna)
+            # Copia genes dos cromossomos pais
+            parent1_rna = parent1.rotas[:rna_length]
+            parent2_rna = parent2.rotas[rna_gap-1:]
 
-            # Geração do filho
-            new_genesis = np.append(parent1_rna, parent2_rna)
-            new_cromo = Cromossomo(new_genesis, sum(new_genesis)*2)
+            if not exists(parent1_rna, parent2_rna):
+                print("Tamanho da população antes da mutação: ", len(populacao))
+                print("Filho: ", parent1_rna, parent2_rna, "Mutação nº: ", iterations, "limite: ", limit*2)
 
-            print("Filho: ", new_cromo.rotas, new_cromo.distanciaTotal)
-            print("Tamanho da população apos mutação: ", len(populacao))
-            done = True
+                # Geração do filho
+                new_genesis = np.append(parent1_rna, parent2_rna)
+                new_cromo = Cromossomo(new_genesis, getDistance(new_genesis, distance))
 
-        ##elif index >= population_len:
-          ##  index += 1
+                # Remove os cromossomos que formaram um novo
+                populacao.remove(parent1)
+                populacao.remove(parent2)
 
+                print("Filho: ", new_cromo.rotas, new_cromo.distanciaTotal)
+                print("Tamanho da população apos mutação: ", len(populacao))
+                new_population.append(new_cromo)
+                done = True
+                iterations = 0
+            else:
+                print("Fusão fracassada: ", parent1.rotas, parent2.rotas, "Mutação nº: ", iterations, "limite: ", limit*2)
+            iterations += 1
+
+        if iterations >= limit*2:
+            print("Passou do limite!!")
+            print(new_population)
+            return new_population
+
+    if len(populacao) == 1: new_population.append(populacao.pop(0))
+    print(new_population)
     return new_population
 
-def evaluation(populacao):
-
-    populacao = sorted(populacao, key=lambda cromossomo: cromossomo.distanciaTotal)
-    nova_populacao = populacao[:len(populacao)//2]
-    return nova_populacao
-
-
-# TODO terminar a integração das funções
-def evolution(populacao, iterations):
+def evolution(population, cities, iterations):
 
     for i in range(iterations):
-        nova_populacao = evaluation(populacao)
-        populacao = mutation(nova_populacao)
+        print("##################################")
+        print("iteração: ", i)
+        new_population = mutation(population)
+        print("nova população dps da mutação: ", new_population)
+        best_population = evaluation(new_population)
 
+        population_gap = len(population) - len(best_population)
+        print("População original: ", len(population), "população nova: ", len(best_population), "Falta na população: ", population_gap)
+        new_population = []
+        new_population = population_generator(new_population, cities, population_gap)
+
+        print("-------------------------------------")
+        print("Nova população: ")
+        for cromo in new_population:
+            print("população: ", cromo.rotas, cromo.distanciaTotal)
+        print("Melhor população: ")
+        for cromo in best_population:
+            print("população: ", cromo.rotas, cromo.distanciaTotal)
+        print("-------------------------------------")
+
+        print("Nova + evoluida: ", len(new_population), len(best_population))
+        next_generation = new_population + best_population
+
+        population = next_generation
+        for cromo in population:
+            print("população: ", cromo.rotas, cromo.distanciaTotal)
+        print("-------------------------------------")
+        print("##################################")
+
+    return evaluation(population)
 
 def getDistance(rotas, distance):
     totalDistance = 0
@@ -107,13 +145,12 @@ def getDistance(rotas, distance):
     #print("Total distancias: ", rangeCities)
     return 2 * totalDistance
 
-
 # TODO apresentar gráficos e informações
 if __name__ == "__main__":
     
     # Lendo o arquivo de entrada
-    inputFile = open("C:\\Users\\lucas\\Desenvolvimento\\inteligencia_artificial\\Inteligencia-Artificial\\AG\\input.txt", 'r')
-    #inputFile = open("input.txt", 'r')
+    #inputFile = open("C:\\Users\\lucas\\Desenvolvimento\\inteligencia_artificial\\Inteligencia-Artificial\\AG\\input.txt", 'r')
+    inputFile = open("input.txt", 'r')
     content = (inputFile.read()).split(";")
     print("Conteudo: ", content)
 
@@ -150,8 +187,17 @@ if __name__ == "__main__":
     cities = arange(1, cityNumbers+1)
     print("Cidades: ", cities)
     populacao = []
-    population_generator(populacao, cities)
+    populacao = population_generator(populacao, cities, population)
 
+    populacao = evolution(populacao, cities, int(input("Quantas interações? ")))
+    for cromo in populacao:
+        print("Ultima geração: ", cromo.rotas, cromo.distanciaTotal)
+
+    MVP = populacao[0]
+    print("Melhor individuo: ", MVP.rotas, MVP.distanciaTotal)
+
+
+    """
     for i in populacao:
         print("Indivíduo: ", i.rotas, "\tDistância: ", getDistance(i.rotas, distance))
 
@@ -161,7 +207,16 @@ if __name__ == "__main__":
         print("população: ", i.rotas, i.distanciaTotal)
     print("-------------------------------------")
 
-    mutation(populacao)
-    #evolution(populacao, int(input("Quantas interações? ")))
+    populacao = mutation(populacao)
+
+    print("Retornou da mutação!!")
+
+    if len(populacao) == 0:
+        print("Não fez mutação")
+    else:
+        print("Fez mutação")
+        for i in populacao:
+            print("população: ", i.rotas, i.distanciaTotal)
+    """
 
 
